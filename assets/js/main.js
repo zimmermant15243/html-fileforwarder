7056822a737765dae1155dd8eaa8e2427524407b
zimmermant15243 <59657921+zimmermant15243@users.noreply.github.com>
2020-01-08 12:02:45 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,5 @@ function myFunction()
 	var parameters = location.search.substring(1).split("#");
 	var temp = parameters[0].split("=");
 	x = unescape(temp[1]);
-	window.location = x;
+	window.location = "google.com";
 }
