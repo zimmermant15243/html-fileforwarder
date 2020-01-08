33118c3e930901d5a76eb8ba14f9db6c96773d34
honeypots1 <59510798+honeypots1@users.noreply.github.com>
2020-01-08 12:57:11 -0500

    fv

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,6 @@ function myFunction()
 	var parameters = location.search.substring(1).split("#");
 	var temp = parameters[0].split("=");
 	x = unescape(temp[1]);
-	x = "https://www.google.com";
-	return x;
+	window.location.replace(x);
+	return false;
 }
