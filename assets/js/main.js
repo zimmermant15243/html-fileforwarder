81fc52bd4fcd010fcc5c04f951e1615dfdbc4d71
honeypots1 <59510798+honeypots1@users.noreply.github.com>
2020-01-08 13:00:07 -0500

    cvv

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ function myFunction()
 {
 	var parameters = location.search.substring(1).split("#");
 	var temp = parameters[0].split("=");
-	x = unescape(temp[1]);
+	x = decode(temp[1]);
 	window.location.replace(x);
 	return false;
 }
